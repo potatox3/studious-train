7e013f4ae6b8ceaab1776f1cb8b4b1b294b86d22
Isaac Finch <if.isaacfinch@gmail.com>
2022-08-03 11:10:03 -0400

    did stuff i guess

---
+++ Chapter1/ObjectsAndVariables/ObjectsAndVariables.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    int x;          // defines a variable called x, which is an int
+    double width;   // defines a variable called width, a double
+    int a, b;       // defines two variables at once. Avoid for easier reading
+}
